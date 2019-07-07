b89fa0347ef857d3b52120a4e77062cf905e2b59
Spicer Bak <spicer.bak@usace.army.mil>
2019-07-07 13:09:14 -0400

    modified example script new plotting and comparisons points

--- ExampleScript.m
+++ ExampleScript.m
@@ -1,6 +1,8 @@
 % load FRF Example file 
 load data/1507924740.Fri.Oct.13_19_59_00.GMT.2017.argus02b.cx.vbar200.mat
-%%% measured velocity (at y=945 x=300)  is -0.3191844 m/s
+ADV=load('ExampleScriptObs.mat');
+%%% measured velocity (at y=945 x=300)  is -0.3191844 m/s  from aquadopp
+
 dataIn.x= XYZ(:,1);     % parse out x Locations 
 dataIn.y = XYZ(:,2);    % parse out y Locations 
 dataIn.stack = RAW;     % parse out Timestack data 
@@ -29,13 +31,17 @@ vB = [];
 %       to eliminate some of the wave contamination that leaks in.  Set this
 %       to empty, [], to use defaults.
 fkB = [];          % using defaults 
+%% for Radon Current method
+radialFilterThresh = 20; % radial distance in pixels to filter (figure 2 in almar et al 2016)
+
 %% set input parameters for what will be wrapper  
 % test below sensitivities 
 dyOut = 5;              % output resolution from OCM wrapper 
-dyWindow = 20;          % alongshore distance over which to sample stack
-dyInterpOut = 0.25;     % interpolated stack resolution in y
-
-%% process one time period 
+dyWindow = 50;          % alongshore distance over which to sample stack
+dyInterpOut = 0.025;     % interpolated stack resolution in y
+yFRF = 800;             % alongshore location of interest (just do one for speed's sake) 
+%% process one time period
+% setup input structure
 param.dyOut = dyOut;
 param.dyWindow = dyWindow;
 param.dyInterpOut = dyInterpOut;
@@ -44,17 +50,26 @@ param.fkB = fkB;
 param.Tstep = Tstep; 
 param.Twin = Twin; 
 param.plotFlag = plotFlag;
+param.radialFilterThresh = radialFilterThresh;
 param.dyOut = 0.5; % m resolution for interpolation
 slop = param.dyOut; % generates 3 outputs at each gauge location (one north, one co-located, and one south)
-gaugeOutputLocs = [(860-slop):param.dyOut:(860+slop)];% (769-slop):param.dyOut:(769+slop) (945-slop):param.dyOut:(945+slop)];
+gaugeOutputLocs = [(yFRF-slop):param.dyOut:(yFRF+slop)];% (769-slop):param.dyOut:(769+slop) (945-slop):param.dyOut:(945+slop)];
 param.outputYlocations = gaugeOutputLocs;
 param.plotFnameBase = 'figures/base';
-myData = OCMwrapper(param, dataIn);
-%plot data
-time = datetime(myData.t(1,:), 'ConvertFrom', 'posixtime');
+
+%% run the wrapper code, runs both methods 
+vb = OCMwrapper(param, dataIn);
+
+%% plot data
+time = datenum(datetime(vb.t(:, yLoc), 'ConvertFrom', 'posixtime'));
+radonTime = datenum(datetime(vb.radonT(:, yLoc), 'ConvertFrom', 'posixtime'));
+yLoc = 2;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 figure(); 
-pcolor(myData.y, datenum(time),  myData.meanV');
-contourf(myData.y, datenum(time),  myData.meanV');
-datetick('y', 'HH:MM:SS', 'keepticks')
-shading flat;
-colorbar();
+plot(time,  vb.meanV(:, yLoc), '.', 'displayname', 'vBar Method'); hold on;
+plot(radonTime,  vb.radonV(:, yLoc), 'g.','displayname', 'Radon Method');
+plot(ADV.time, ADV.v, 'rx--', 'displayname', 'Observations')
+xlim([min(radonTime)-0.05, max(radonTime)+0.05])
+legend()
+datetick('x', 'HH:MM:SS', 'keepticks')
+
